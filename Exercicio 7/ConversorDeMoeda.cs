a9dc5927c8c63874518bb7e01c873f2fc06ba80f
Quiel <Quiel@DESKTOP-QPM4FGN>
2021-12-05 10:38:19 -0300

    Add project files.

---
+++ Exercicio 7/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio_7
+{
+    static public class ConversorDeMoeda
+    {
+        //Conversão da moeda com + 6% de IOF
+        static public double Conversao(double cotacao, double dolares)
+        {
+            double valor = cotacao * dolares;
+            double resultado = ((valor * 6) / 100) + valor;
+            return resultado;
+        }
+    }
+}
---
+++ Exercicio 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão da moeda com + 6% de IOF
+            Console.WriteLine("Qual é a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double dolares = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nValor a ser pago em reais = " +
+                $"{ConversorDeMoeda.Conversao(cotacao, dolares).ToString("F2")}");  
+
+            Console.WriteLine("\nPressione qualquer tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
